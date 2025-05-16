3b037dff2e2004e1f38949c80d051a5abaff55ea
SUJAN GANIGA <sujan.ganiga2020@gmail.com>
2025-05-16 23:43:10 +0530

    suj

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -1,20 +1,16 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
   name: String,
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   phone: String,
-  role: { type: String, enum: ['driver', 'passenger'], default: 'passenger' },
+  role: { type: String, enum: ["driver", "passenger"], default: "passenger" },
   joinedAt: { type: Date, default: Date.now },
-  Adaarnumber:{
-    type:Number,
-    required:true
-  }
+  Adaarnumber: {
+    type: Number,
+  },
 });
 
-
-
-const usermodel=mongoose.models.User || mongoose.model('User', UserSchema);
+const usermodel = mongoose.models.User || mongoose.model("User", UserSchema);
 export default usermodel;
-
